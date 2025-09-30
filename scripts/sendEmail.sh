c0f7ce084d6290aeb93dc096daf5c8d9b7563740
CaioBonvicine <caiopbonvicine@gmail.com>
2025-09-30 11:52:27 -0300

    fix: working pipeline as intended (no hardcode email)

--- scripts/sendEmail.sh
+++ scripts/sendEmail.sh
@@ -1,3 +1,3 @@
 cd bilioteca
 sudo apt-get install mailutils
-echo "Sending e-mail after pipleline completion" | mail -s "Pipeline" gab.alves.pereira30@gmail.com
+echo "Sending e-mail after pipleline completion" | mail -s "Pipeline" "$EMAIL_RECIPIENT"
